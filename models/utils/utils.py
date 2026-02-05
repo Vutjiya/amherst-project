b20c8124ae6fd1a63d25f9d58bfcace4f6517c01
jonathanliu72 <jonathan.liu72@gmail.com>
2026-02-05 00:17:49 -0500

    debugging and implementing HOG descriptors

--- models/utils/utils.py
+++ models/utils/utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from PIL import Image
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 #TODO: preprocess images so all of them are square
 
@@ -33,26 +34,54 @@ def process_and_split_images():
     towns = ['amherst', 'annapolis', 'annarbor', 'cambridge', 'charlottesville', 'hanover', 'newhaven', 'oberlin', 'princeton']
 
     for town in towns:
-        path = Path.cwd().parent.parent.joinpath(f'dataset_tool/cutouts/{town}')
+        path = Path.cwd().joinpath(f'data/cutouts/{town}_cutouts')
         paths[town] = path
 
     # Create discovery sets D1, D2
     discovery_set = []
-    for image in paths['amherst'].iterdir():
-        img = Image.open(image)
+    p = 0
+    for path_to_image in paths['amherst'].iterdir():
+        img = Image.open(path_to_image)
         matrix = np.array(img)
         discovery_set.append(matrix)
+        if p==1:
+            break
+        p+=1
     
     discovery_set1, discovery_set2 = train_test_split(discovery_set, test_size=0.5, random_state=67)
 
     # Create natural world sets W1, W2
+    q = 0
     natural_world_set = []
-    for image in paths.values().iterdir():
-        img = Image.open(image)
-        matrix = np.array(img)
-        natural_world_set.append(matrix)
+    for city in paths.values():
+        for path_to_image in city.iterdir():
+            img = Image.open(path_to_image)
+            matrix = np.array(img)
+            print(matrix.shape)
+            natural_world_set.append(matrix)
+            if q==1:
+                break
+            q+=1
     
     natural_world_set1, natural_world_set2 = train_test_split(natural_world_set, test_size=0.5, random_state=42)
     
     return [discovery_set1, discovery_set2], [natural_world_set1, natural_world_set2]
+
+
+def sample_patches():
+    patches = []
+
+    D, N = process_and_split_images()
+
+    for image in D[1]:
+        print()
+
+def main():
+    D, N = process_and_split_images()
+    #print(D[0])
+
+
+if __name__ == '__main__':
+    main()
+
 
